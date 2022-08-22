6548ed309cf03fddf14728a667af8974085b914c
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2022-08-22 15:41:41 -0600

    Working install of material-ui-uxpin-1.0.0.tgz

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import {Button} from 'material-ui-uxpin';
+import {UXPinWrapper as ThemeProvider} from 'material-ui-uxpin';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider>
+     <Button variant="contained">Click me!</Button>
+     </ThemeProvider>
     </div>
   );
 }
