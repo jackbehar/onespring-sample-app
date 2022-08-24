8a73037fd78d6bc27e407f14376b871153a9ea7c
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2022-08-24 17:09:54 -0600

    Updated package and app.js with MenuList

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {Button} from 'material-ui-uxpin';
+import {Button, MenuList, MenuItem, Paper} from 'material-ui-uxpin';
 import {UXPinWrapper as ThemeProvider} from 'material-ui-uxpin';
 
 function App() {
@@ -7,9 +7,24 @@ function App() {
     <div className="App">
       <ThemeProvider>
      <Button variant="contained">Click me!</Button>
+     <Paper>
+  <MenuList>
+    <MenuItem>
+      Profile
+    </MenuItem>
+    <MenuItem>
+      My account
+    </MenuItem>
+    <MenuItem>
+      Logout
+    </MenuItem>
+  </MenuList>
+</Paper>
      </ThemeProvider>
     </div>
   );
 }
+
+
 
 export default App;
